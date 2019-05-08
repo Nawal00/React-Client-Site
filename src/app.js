e8e23d7cdbfee5b609e4936751735f27d53508fd
Nawal00 <nawalgurung80@gmail.com>
2019-05-08 13:11:53 +0100

    added bootstrap

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import  'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './sass/style.sass'
 
 import Navbar from './components/Navbar'
