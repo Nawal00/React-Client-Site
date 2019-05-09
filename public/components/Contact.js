a909ec681e8e53cde1d92d0674d7dc44ae505b2c
Nawal00 <nawalgurung80@gmail.com>
2019-05-09 22:05:10 +0100

    contact form styled

--- public/components/Contact.js
+++ public/components/Contact.js
@@ -10,7 +10,7 @@ class Contact extends React.Component {
       name: '',
       message: '',
       email: '',
-      send: false,
+      sent: false,
       buttonText: 'Send Message'
     }
     this.handleChange = this.handleChange.bind(this)
@@ -24,56 +24,79 @@ class Contact extends React.Component {
     this.setState({[name]: value})
   }
 
-  resetForm(){
-    this.setState({
-      name: '',
-      message: '',
-      email: '',
-      buttonText: 'Message Sent',
-      send: true
-    })
-  }
-
   formSubmit(e){
     e.preventDefault()
     this.setState({ buttonText: '...sending' })
     const { name, message, email} = this.state
 
     axios.post('/api/submit', { name, message, email })
-      .then(() => this.setState(this.resetForm()))
+      .then(this.resetForm())
       .catch(() => console.log('Message not sent'))
+  }
+
+  resetForm(){
+    this.setState({
+      name: '',
+      message: '',
+      email: '',
+      buttonText: 'Message Sent',
+      phone: '',
+      company: '',
+      sent: true
+    })
   }
 
   render () {
     return (
       <div id="contact">
-        <form onSubmit={this.formSubmit} className="container">
-          <div className="col-sm-6">
-            <h3 className=""> Get in touch </h3>
-            <div className="form-group">
-              <label className="message" htmlFor="message-input">Message</label>
+      
+        <form onSubmit={this.formSubmit} className="container card">
+          <div className="company-info">
+            <h3>Get in touch</h3>
+            <ul>
+              <li><i className="fa fa-road"></i>  44 Something st</li>
+              <li><i className="fa fa-phone"></i>  (+447) 555-5555</li>
+              <li><i className="fa fa-envelope"></i>  test@email.test</li>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label>Name</label>
+              <input onChange={this.handleChange} name="name" className="form-control" type="text" placeholder="name" value={this.state.name}/>
+            </div>
+
+            <div className="col">
+              <label>Email</label>
+              <input onChange={this.handleChange} name="email" className="form-control" type="email" placeholder="your@email.com" value={this.state.email} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <label>Phone</label>
+              <input onChange={this.handleChange} name="phone" className="form-control" type="number" placeholder="+447 000 000" value={this.state.phone} />
+            </div>
+
+            <div className="col">
+              <label>Company</label>
+              <input onChange={this.handleChange} name="company" className="form-control" type="text" placeholder="company" value={this.state.company} />
+            </div>
+          </div>
+
+          <div className="col-12">
+            <div className="row">
+              <label>Message</label>
               <textarea onChange={this.handleChange} name="message" className="form-control" type="text" placeholder="Please write your message here" value={this.state.message} required
               />
             </div>
           </div>
 
-          <div className="form-group">
-            <div className="col-sm-6">
-              <label className="message-name" htmlFor="message-name">Name</label>
-              <input onChange={this.handleChange} name="name" className="form-control" type="text" placeholder="Your Name" value={this.state.name}/>
+          <div className="col-12 btn">
+            <div className="row">
+              <button type="submit" className= { this.state.buttonText === 'Message Sent'? 'btn btn-success btn-block' : 'btn btn-danger btn-block' }>{ this.state.buttonText }</button>
             </div>
           </div>
 
-          <div className="form-group">
-            <div className="col-sm-6">
-              <label className="message-email" htmlFor="message-email">Email</label>
-              <input onChange={this.handleChange} name="email" className="form-control" type="email" placeholder="your@email.com" value={this.state.email} />
-            </div>
-          </div>
-
-          <div className="col-sm-6">
-            <button type="submit" className= {this.state.buttonText === 'Message Sent'? 'btn btn-success' : 'btn btn-danger' }>{ this.state.buttonText }</button>
-          </div>
         </form>
       </div>
     )
